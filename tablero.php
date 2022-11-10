b3aa8f0e614e6731d9a7f0532f8ae5484f30dd1e
fcaalexia <89996255+fcaalexia@users.noreply.github.com>
2022-11-10 15:29:43 -0600

    Add files via upload

---
+++ tablero.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="tablero">
+       <div class="a">🚢</div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a">🚢</div>
+       <div></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a">🚢</div>
+       <div></div>
+       <div class="a">🚢</div>
+       <div></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div ></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+   </div>
+    <br><br><br><br>
+   <div class="tablero_dos">
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a">🚢</div>
+       <div></div>
+       <div class="a"></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a">🚢</div>
+       <div></div>
+       <div class="a"></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a">🚢</div>
+       <div></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a">🚢</div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div ></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a">🚢</div>
+       <div></div>
+       <div class="a"></div>
+       <div></div>
+       <div class="a"></div>
+   </div>
+</body>
+</html>
